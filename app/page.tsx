8231c0c4cd0de79546fc91d588de74aba6be9fea
Ali Al-Shaikh <mi3lix9@outlook.sa>
2024-07-07 03:41:58 +0300

    integrate db with fromnt end

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
+import { collection, getDocs } from "firebase/firestore";
 import { Store } from "./components/store";
+import { db } from "./utils/firebase";
 
-export default function Home() {
+export default async function Home() {
   const imageUrl = "https://logowik.com/content/uploads/images/firebase.jpg";
-  const stores = [
-    { id: 1, name: "الفرات", imageUrl },
-    { id: 2, name: "زهرة الخلد", imageUrl },
-    { id: 2, name: "تغميس", imageUrl },
-    { id: 2, name: "شطة", imageUrl },
-    { id: 2, name: "معطم حسن بشير", imageUrl },
-    { id: 2, name: "التنور", imageUrl },
-    { id: 2, name: "الشيخ", imageUrl },
-    { id: 2, name: "قصر مايتي", imageUrl },
-  ];
+  const stores = await getAllStores();
   return (
     <div className="container mx-auto px-4 flex gap-2 select-none flex-wrap">
       {stores.map((s) => (
@@ -19,4 +12,24 @@ export default function Home() {
       ))}
     </div>
   );
+}
+
+type Store = {
+  id: string;
+  name: string;
+  imageUrl: string;
+};
+
+async function getAllStores(): Promise<Store[]> {
+  const storesCollectionRef = collection(db, "stores");
+  const storeSnapshot = await getDocs(storesCollectionRef);
+  const storesList: Store[] = storeSnapshot.docs.map((doc) => {
+    const data = doc.data();
+    return {
+      id: doc.id,
+      name: data.name,
+      imageUrl: data.imageUrl,
+    };
+  });
+  return storesList;
 }
--- app/stores/[id]/page.tsx
+++ app/stores/[id]/page.tsx
@@ -1,20 +1,72 @@
+import { Store } from "@/app/components/store";
+import { db } from "@/app/utils/firebase";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  getDoc,
+} from "firebase/firestore";
+
+type ProductType = { id: string | number; name: string; imageUrl: string };
 type StoreType = {
   id: string | number;
   name: string;
   imageUrl: string;
-  products: { id: string | number; name: string; imageUrl: string }[];
+  products: ProductType[];
 };
 
-const Store: React.FC<StoreType> = (props) => {
+// const Store: React.FC<StoreType> = (props) => {
+//   const store = await getStore(props.);
+// };
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const store = await getStore(params.id);
+
   return (
     <main className="px-2">
       <img
-        src={props.imageUrl}
-        alt={props.name}
+        src={store.imageUrl}
+        alt={store.name}
         className="w-full aspect-square rounded-md"
       />
+
+      {store.products?.map((product) => (
+        <Store
+          id={product.id}
+          imageUrl={product.imageUrl}
+          name={product.name}
+        />
+      ))}
     </main>
   );
-};
+}
 
-export default Store;
+async function getStore(storeId: string | number): Promise<StoreType> {
+  const storeDocRef = doc(db, "stores", storeId.toString());
+  const storeDoc = await getDoc(storeDocRef);
+
+  if (storeDoc.exists()) {
+    const storeData = storeDoc.data();
+
+    const productsCollectionRef = collection(storeDocRef, "products");
+    const productsSnapshot = await getDocs(productsCollectionRef);
+    const productsList: ProductType[] = productsSnapshot.docs.map((doc) => {
+      const productData = doc.data();
+      return {
+        id: doc.id,
+        name: productData.name,
+        imageUrl: productData.imageUrl,
+      };
+    });
+
+    return {
+      id: storeDoc.id,
+      name: storeData.name,
+      imageUrl: storeData.imageUrl,
+      products: productsList,
+    } as StoreType;
+  } else {
+    throw new Error("No such document!");
+  }
+}
